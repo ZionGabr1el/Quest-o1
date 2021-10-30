577ee43f59cfc040ee262de799ace272c1651045
Zion <89410103+ZionGabr1el@users.noreply.github.com>
2021-10-29 23:47:52 -0300

    Add files via upload

---
+++ Controle.java
@@ -0,0 +1,19 @@
+package questão1;
+
+public class Controle {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+      
+        c.maisVolume();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+        
+        
+     }
+     
+ }
+ 
+
